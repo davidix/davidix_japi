6cd02d68a699fd9788b5e4218211597d9de82ea0
davidix <d.bereihi@gmail.com>
2022-08-01 23:10:21 +0430

    Update davidix_japi.php

--- davidix_japi.php
+++ davidix_japi.php
@@ -4,31 +4,22 @@ require('controller.php');
 
 class plgSystemDavidix_japi extends JPlugin
 {
-	
 	 public function onAfterInitialise()
 	 {
-		 	$app 	=	JFactory::getApplication();
-			$opt 	=	$app->input->get('opt', '', 'string');
-			$Rest	=	new dixRest();
-			
-			switch($opt)
-			{
-				case "items" :
+		$app 	=	JFactory::getApplication();
+		$opt 	=	$app->input->get('opt', '', 'string');
+		$Rest	=	new dixRest();
+		
+		switch($opt)
+		{
+			case "items" :
 				$Rest->items();
 				break;
-				
-				case "login" :
+			
+			case "login" :
 				$Rest->login();
 				break;
-				
-			}
 			
-			
-		
+		}
 	 }
-	 
-
-	 
-	
-   
 }
